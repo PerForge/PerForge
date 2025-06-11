b4cb18e06599fcb2d7ebf1af6f37e64f11b40899
ushklianik <ushklianik@gmail.com>
2025-06-11 10:08:15 +0300

    Updated pydantic version

--- app/backend/integrations/data_sources/influxdb_v2/influxdb_extraction.py
+++ app/backend/integrations/data_sources/influxdb_v2/influxdb_extraction.py
@@ -19,7 +19,7 @@ from app.backend.integrations.data_sources.base_extraction                      
 from app.backend.integrations.data_sources.base_queries                                         import QueriesBase
 from app.backend.integrations.data_sources.influxdb_v2.queries.influxdb_backend_listener_client import InfluxDBBackendListenerClientImpl
 from app.backend.integrations.data_sources.influxdb_v2.queries                                  import mderevyankoaqa
-from app.backend.integrations.data_sources.influxdb_v2.queries.sitespeed_influxdb_v2            import Sitespeed
+from app.backend.integrations.data_sources.influxdb_v2.queries.sitespeed_influxdb_v2            import SitespeedFluxQueries
 from app.backend.integrations.data_sources.influxdb_v2.influxdb_db                              import DBInfluxdb
 from app.backend.components.secrets.secrets_db                                                  import DBSecrets
 from app.backend.errors                                                                         import ErrorMessages
@@ -35,7 +35,7 @@ class InfluxdbV2(DataExtractionBase):
     queries_map: Dict[str, Type[QueriesBase]] = {
         "org.apache.jmeter.visualizers.backend.influxdb.InfluxdbBackendListenerClient": InfluxDBBackendListenerClientImpl,
         "mderevyankoaqa": mderevyankoaqa,
-        "sitespeed_influxdb_v2": Sitespeed
+        "sitespeed_influxdb_v2": SitespeedFluxQueries
     }
 
     def __init__(self, project, id=None):
@@ -99,7 +99,6 @@ class InfluxdbV2(DataExtractionBase):
             records = self._execute_query(query)
 
             df = pd.DataFrame(records)
-            print(df)
             sorted_df = df.sort_values(by='start_time')
             return sorted_df.to_dict(orient='records')
         except Exception as er:
